fd909170ea193cd73ae604bc843f08b55d135327
Siddharth1097 <siddarth@majjaka.com>
2023-12-20 14:01:17 +0530

    fix

--- eProc_Marketing/views.py
+++ eProc_Marketing/views.py
@@ -13,18 +13,9 @@ from io import StringIO
 
 from django.views.decorators.csrf import csrf_exempt
 from flask.app import Flask
-from pyvirtualdisplay import Display  # Import the Display class from pyvirtualdisplay
 
 app = Flask(__name__)
-display_var = os.environ.get('DISPLAY', None)
 
-# Create a virtual display only if DISPLAY is available
-if display_var:
-    display = Display()
-    display.start()
-else:
-    display = None
-    print("DISPLAY environment variable not set. Skipping virtual display.")
 
 def index(request):
     context = {
@@ -34,6 +25,7 @@ def index(request):
         'is_configuration_active': True
     }
     return render(request, 'marketing.html', context)
+
 
 def send_whatsapp_message(phone_number, message, image_path, send_time):
     try:
@@ -67,6 +59,7 @@ def send_whatsapp_message(phone_number, message, image_path, send_time):
         import traceback
         traceback.print_exc()
 
+
 @csrf_exempt
 def send_message(request):
     global image_path
@@ -88,6 +81,7 @@ def send_message(request):
         csv_content = csv_file.read().decode('utf-8', errors='replace')
 
         # Use StringIO to create a file-like object for csv.reader
+        from io import StringIO
         text_csv_file = StringIO(csv_content)
 
         # Read phone numbers from the CSV file
@@ -113,14 +107,6 @@ def send_message(request):
         send_time = now.replace(hour=start_hours, minute=start_minutes, second=0, microsecond=0) + datetime.timedelta(
             seconds=5)
 
-        # Create a virtual display only if DISPLAY is available
-        display = None
-        try:
-            display = Display()
-            display.start()
-        except Exception as e:
-            print(f"Error starting virtual display: {e}")
-
         # Call the existing function for each phone number
         for phone_number in phone_numbers:
             try:
@@ -137,14 +123,11 @@ def send_message(request):
             except Exception as e:
                 print(f'Error sending message to {phone_number}: {str(e)}')
 
-        # Stop the virtual display if started
-        if display:
-            display.stop()
-
         return JsonResponse({'result': 'Messages sent successfully'})
     except Exception as e:
         print(f'Error: {str(e)}')
         return JsonResponse({'result': f'Error: {str(e)}'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)

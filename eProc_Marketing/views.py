0e7cddcfc33b3893339c1570f85f80d45bd7562a
Siddharth1097 <siddarth@majjaka.com>
2023-12-20 13:54:51 +0530

    fix

--- eProc_Marketing/views.py
+++ eProc_Marketing/views.py
@@ -13,9 +13,9 @@ from io import StringIO
 
 from django.views.decorators.csrf import csrf_exempt
 from flask.app import Flask
+from pyvirtualdisplay import Display  # Import the Display class from pyvirtualdisplay
 
 app = Flask(__name__)
-
 
 def index(request):
     context = {
@@ -25,7 +25,6 @@ def index(request):
         'is_configuration_active': True
     }
     return render(request, 'marketing.html', context)
-
 
 def send_whatsapp_message(phone_number, message, image_path, send_time):
     try:
@@ -59,7 +58,6 @@ def send_whatsapp_message(phone_number, message, image_path, send_time):
         import traceback
         traceback.print_exc()
 
-
 @csrf_exempt
 def send_message(request):
     global image_path
@@ -81,7 +79,6 @@ def send_message(request):
         csv_content = csv_file.read().decode('utf-8', errors='replace')
 
         # Use StringIO to create a file-like object for csv.reader
-        from io import StringIO
         text_csv_file = StringIO(csv_content)
 
         # Read phone numbers from the CSV file
@@ -107,27 +104,28 @@ def send_message(request):
         send_time = now.replace(hour=start_hours, minute=start_minutes, second=0, microsecond=0) + datetime.timedelta(
             seconds=5)
 
-        # Call the existing function for each phone number
-        for phone_number in phone_numbers:
-            try:
-                # Send both text message and image at the same time
-                send_whatsapp_message(phone_number, message, image_path, send_time)
+        # Create a virtual display
+        with Display():
+            # Call the existing function for each phone number
+            for phone_number in phone_numbers:
+                try:
+                    # Send both text message and image at the same time
+                    send_whatsapp_message(phone_number, message, image_path, send_time)
 
-                # Increment send_time for the next contact
-                send_time += interval
+                    # Increment send_time for the next contact
+                    send_time += interval
 
-                # Handle hour transition
-                if send_time.minute >= 60:
-                    send_time = send_time.replace(hour=send_time.hour + 1, minute=send_time.minute % 60)
+                    # Handle hour transition
+                    if send_time.minute >= 60:
+                        send_time = send_time.replace(hour=send_time.hour + 1, minute=send_time.minute % 60)
 
-            except Exception as e:
-                print(f'Error sending message to {phone_number}: {str(e)}')
+                except Exception as e:
+                    print(f'Error sending message to {phone_number}: {str(e)}')
 
         return JsonResponse({'result': 'Messages sent successfully'})
     except Exception as e:
         print(f'Error: {str(e)}')
         return JsonResponse({'result': f'Error: {str(e)}'})
-
 
 if __name__ == '__main__':
     app.run(debug=True)

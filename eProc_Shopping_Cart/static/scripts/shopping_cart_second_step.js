c9d73cee56df6f6ba5a49db91da482e2a23b38e9
Ranjithprasanth07 <ranjith@majjaka.com>
2024-02-26 09:52:50 +0530

    fix

--- eProc_Shopping_Cart/static/scripts/shopping_cart_second_step.js
+++ eProc_Shopping_Cart/static/scripts/shopping_cart_second_step.js
@@ -161,16 +161,12 @@ function edit_sc_name(){
 
 // Function to submit edit SC name
 function submitSCname(){
-   var errorMessageContainer = document.getElementById('error-message');
-   var sc_name = document.getElementById('sc_name_input').value
-   if(sc_name == ""){
-       errorMessageContainer.textContent = 'Please enter valid name';
-       $('#edit_sc_name_window').modal('show');
-   }else {
-       errorMessageContainer.textContent = '';
-       document.getElementById('sc_name_id').innerHTML = sc_name
-       $('#edit_sc_name_window').modal('hide');
-   }
+    var sc_name = document.getElementById('sc_name_input').value
+
+    document.getElementById('sc_name_id').innerHTML   = sc_name
+
+    $('#edit_sc_name_window').modal('hide');
+}
 }
 
 
